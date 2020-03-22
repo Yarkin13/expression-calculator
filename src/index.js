8b306c1a01a6efc5500703eb4bc15a1719aac71e
Yarkin13 <stas20072008@yandex.ru>
2020-03-22 20:51:24 +0400

    feat: add solutions for brackets

--- src/index.js
+++ src/index.js
@@ -3,15 +3,64 @@ function eval() {
     return;
 }
 
-    function expressionCalculator(expr) {
+function expressionCalculator(expr) {
+    let openBrackets=0;
+    let closeBrackets=0;
+    let inBrackets=[];
+    for (let i=0; i<expr.length; i++) {
+        if(expr[i].search(/\)/)==!-1) openBrackets++;
+        if(expr[i].search(/\(/)==!-1) closeBrackets++;
+    } //считаю скобки
+    if(openBrackets!=closeBrackets) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    } //непарные скобки
     expr = expr.trim()
     let arr= expr.split(' ');
-    if (arr[0]=="") arr.splice(0, 1);
-    if (arr[0]=="") arr.splice(-1, 1);
     let preresult=0;
+    let k=0;
+    while(k<arr.length) {
+        if(arr[k]=='') {
+            arr.splice(k,1);
+        } else k++;
+    } //убираю пустые элементы массива
     for(let i=0; i<arr.length; i++) {
-        if(arr[i].search(/\d/)==!-1) arr[i]=Number(arr[i])
+        if(arr[i].search(/\d/)==!-1) arr[i]=Number(arr[i]) //перевод операндов в числа
     }
+    let openBracket=arr.indexOf('(')
+    let closeBracket=arr.indexOf(')')//ищу индекс открытой и закрытой скобки
+    inBrackets=arr.slice(openBracket,closeBracket+1)//извлекаю массив элементов которые были в скобках
+    if (inBrackets.length){
+        arr.splice(openBracket,closeBracket-openBracket+1)
+        inBrackets.splice(0,1);
+        inBrackets.splice(-1,1);//убираю скобки из массива
+        let q=0;
+        while(q<inBrackets.length) {
+            if(inBrackets[q]=="*") {
+                preresult=inBrackets[q-1]*inBrackets[q+1];
+                inBrackets.splice(q-1,3,preresult);
+                q=0;
+            } else if(inBrackets[q]=="/") {
+                if(inBrackets[q+1]==0) throw  new  Error("TypeError: Division by zero.");//деление на 0
+                preresult=inBrackets[q-1]/inBrackets[q+1];
+                inBrackets.splice(q-1,3,preresult);
+                q=0;
+            } else q++;
+        }
+        let w=0;
+        while(w<inBrackets.length) {
+            if(inBrackets[w]=="+") {
+                preresult=inBrackets[w-1]+inBrackets[w+1];
+                inBrackets.splice(w-1,3,preresult);
+                w=0;
+            } else if(inBrackets[w]=="-") {
+                preresult=inBrackets[w-1]-inBrackets[w+1];
+                inBrackets.splice(w-1,3,preresult);
+                w=0;
+            }   else w++;
+        }   
+        arr.splice(openBracket,0,preresult)     
+    }
+    
     let i=0;
     while(i<arr.length) {
         if(arr[i]=="*") {
@@ -19,7 +68,7 @@ function eval() {
             arr.splice(i-1,3,preresult);
             i--;
         } else if(arr[i]=="/") {
-            if(arr[i+1]==0) throw  new  Error("TypeError: Division by zero.");
+            if(arr[i+1]==0) throw  new  Error("TypeError: Division by zero.");//деление на 0
             preresult=arr[i-1]/arr[i+1];
             arr.splice(i-1,3,preresult);
             i--;
@@ -32,14 +81,16 @@ function eval() {
             arr.splice(j-1,3,preresult);
             j--;
         } else if(arr[j]=="-") {
-            if(arr[j+1]==0) throw  new  Error("TypeError: Division by zero.");
+            if(arr[j+1]==0);
             preresult=arr[j-1]-arr[j+1];
             arr.splice(j-1,3,preresult);
             j--;
         } else j++;
     }
     return Number(arr.join());
+    
 }
+
 
 module.exports = {
     expressionCalculator
